338851279d65fb5e349f5bb709867d581fdba8ea
kronosGR <geo.elgeo@gmail.com>
2022-06-24 07:50:41 +0200

    event and attribute binding

--- reverse-phrase/src/app/app.component.ts
+++ reverse-phrase/src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'reverse-phrase';
+  text = '';
+
+  onClickReverse() {
+    console.log('Clicked');
+  }
+
+  onInputText(event: any) {
+    this.text = (event.target as HTMLInputElement).value;
+  }
 }
