6d7df2c7b3f63bb27b77008174a560afd48df7c6
kronosGR <geo.elgeo@gmail.com>
2022-06-25 12:32:01 +0200

    pass data to child

--- components/src/app/app.component.ts
+++ components/src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'components';
+  posts = [
+    {
+      img: 'assets/image1.jpg',
+      author: 'John',
+      title: 'Title #1',
+    },
+    {
+      img: 'assets/image2.jpg',
+      author: 'Mike',
+      title: 'Title #2',
+    },
+    {
+      img: 'assets/image3.jpg',
+      author: 'Jane',
+      title: 'Title #3',
+    },
+  ];
 }
