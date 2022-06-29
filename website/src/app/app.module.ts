34b261e4c672620b3061485152e7cc980e6672fe
kronosGR <geo.elgeo@gmail.com>
2022-06-29 08:05:00 +0200

    added about and contact routes/components

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
+import {ContactModule} from "./contact/contact.module";
+import {AboutModule} from "./about/about.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HomeModule],
+  imports: [BrowserModule, AppRoutingModule, HomeModule,ContactModule,AboutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
