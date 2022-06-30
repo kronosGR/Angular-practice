09e0e41a6374a44d2c5233131a3e18a5a46c26af
kronosGR <geo.elgeo@gmail.com>
2022-06-30 07:17:40 +0200

    routerLink

--- website/src/app/app-routing.module.ts
+++ website/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ContactModule } from './contact/contact.module';
 
 const routes: Routes = [
   {
