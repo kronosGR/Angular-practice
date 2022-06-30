1cbfff2d7fb9abbb07bd2b776164222ed7e63a60
kronosGR <geo.elgeo@gmail.com>
2022-06-30 07:10:04 +0200

    added more components and children routes

--- website/src/app/about/about-routing.module.ts
+++ website/src/app/about/about-routing.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {AboutMainComponent} from "./about-main/about-main.component";
+import { AboutMainComponent } from './about-main/about-main.component';
+import { BioComponent } from './bio/bio.component';
+import { TeamComponent } from './team/team.component';
+import { ClientsComponent } from './clients/clients.component';
 
 const routes: Routes = [
-  {path:'about', component:AboutMainComponent}
+  {
+    path: 'about',
+    component: AboutMainComponent,
+    children: [
+      { path: '', component: BioComponent },
+      { path: 'team', component: TeamComponent },
+      { path: 'clients', component: ClientsComponent },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AboutRoutingModule { }
+export class AboutRoutingModule {}
