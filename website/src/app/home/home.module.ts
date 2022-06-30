465d45d8a3ef7072ccae87276fc975837633ccab
kronosGR <geo.elgeo@gmail.com>
2022-06-30 07:01:50 +0200

    added base module and alert component and use of ng-content

--- website/src/app/home/home.module.ts
+++ website/src/app/home/home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeMainComponent } from './home-main/home-main.component';
+import { BaseModule } from '../base/base.module';
 
 @NgModule({
   declarations: [HomeMainComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, BaseModule],
   exports: [],
 })
 export class HomeModule {}
