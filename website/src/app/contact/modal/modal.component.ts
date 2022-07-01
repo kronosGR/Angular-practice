776e15a94c3500e7c205bb0d6dbed78a35895945
kronosGR <geo.elgeo@gmail.com>
2022-07-01 08:03:45 +0200

    move the modal to the body

--- website/src/app/contact/modal/modal.component.ts
+++ website/src/app/contact/modal/modal.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent implements OnInit {
-
-  constructor() { }
+  constructor(private el: ElementRef) {}
 
   ngOnInit(): void {
+    // move the element if exists, if not create it.
+    // this.el.nativeElement point on where the element is stored
+    document.body.appendChild(this.el.nativeElement);
   }
 
+  ngOnDestroy(): void {
+    this.el.nativeElement.remove();
+  }
 }
