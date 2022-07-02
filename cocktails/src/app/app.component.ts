b9ba4aeecf145e55bb1a25e53a6d6c0474af330b
kronosGR <geo.elgeo@gmail.com>
2022-07-02 08:07:08 +0200

    added cocktail service and injected to app.component.ts

--- cocktails/src/app/app.component.ts
+++ cocktails/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CocktailService } from './cocktail.service';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cocktails';
+  constructor(private cocktail: CocktailService) {}
 
   search(query: string) {
     console.log(query);
