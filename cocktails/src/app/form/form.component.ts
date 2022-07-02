a7afbff1b7a367089ef926a0611d3362e0bcb4eb
kronosGR <geo.elgeo@gmail.com>
2022-07-02 08:05:07 +0200

    get the form details and handle submit from parent

--- cocktails/src/app/form/form.component.ts
+++ cocktails/src/app/form/form.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
+  @Output() submitted = new EventEmitter();
+  query = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(event: Event) {
+    event.stopPropagation();
   }
 
+  updateQuery(event: Event) {
+    this.query = (event.target as HTMLInputElement).value;
+    this.submitted.emit(this.query);
+  }
 }
