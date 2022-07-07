b88a7886b4aa61de139c41eb3f8243cbb42f8fb3
kronosGR <geo.elgeo@gmail.com>
2022-07-07 08:24:31 +0200

    injected the validator to registration form

--- auth/src/app/register/register.component.ts
+++ auth/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Match } from '../validators/match';
 
 @Component({
   selector: 'app-register',
@@ -7,25 +8,28 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  registerForm = new FormGroup({
-    email: new FormControl('', [
-      Validators.required,
-      Validators.minLength(5),
-      Validators.maxLength(50),
-    ]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(5),
-      Validators.maxLength(20),
-    ]),
-    passwordConfirmation: new FormControl('', [
-      Validators.required,
-      Validators.minLength(5),
-      Validators.maxLength(20),
-    ]),
-  });
+  registerForm = new FormGroup(
+    {
+      email: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(50),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(20),
+      ]),
+      passwordConfirmation: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(20),
+      ]),
+    },
+    { validators: [this.match.validate] }
+  );
 
-  constructor() {}
+  constructor(private match: Match) {}
 
   ngOnInit(): void {}
 }
--- auth/src/app/validators/match.ts
+++ auth/src/app/validators/match.ts
@@ -1,9 +1,17 @@
 import { AbstractControl, ValidationErrors, Validator } from '@angular/forms';
+import { Injectable } from '@angular/core';
 
+@Injectable({
+  providedIn: 'root',
+})
 export class Match implements Validator {
   registerOnValidatorChange(fn: () => void): void {}
 
   validate(control: AbstractControl): ValidationErrors | null {
-    return null;
+    const { password, passwordConfirmation } = control.value;
+
+    if (password === passwordConfirmation) return null;
+
+    return { mismatch: true };
   }
 }
