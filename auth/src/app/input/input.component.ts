9e368922d1b252928601d869fb14dbd937b907d1
kronosGR <geo.elgeo@gmail.com>
2022-07-09 07:55:06 +0200

    show errors on screen

--- auth/src/app/input/input.component.ts
+++ auth/src/app/input/input.component.ts
@@ -13,4 +13,9 @@ export class InputComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  showMsg() {
+    const { dirty, touched, errors } = this.control;
+    return dirty && touched && errors;
+  }
 }
