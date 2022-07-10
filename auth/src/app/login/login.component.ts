24bd52b651ad8938e320af93e36292bf171d9567
kronosGR <geo.elgeo@gmail.com>
2022-07-10 08:18:37 +0200

    submit the login form

--- auth/src/app/login/login.component.ts
+++ auth/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,11 +8,26 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   loginForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(50),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(20),
+    ]),
   });
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  login() {
+    if (this.loginForm.invalid) return;
+
+    const { email, password } = this.loginForm.value;
+    console.log(email, password);
+  }
 }
