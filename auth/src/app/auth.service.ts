fcfb18201d7d9eee226b80753b0307e40cdd3583
kronosGR <geo.elgeo@gmail.com>
2022-07-10 08:26:06 +0200

    subscribe to authstate observable

--- auth/src/app/auth.service.ts
+++ auth/src/app/auth.service.ts
@@ -5,7 +5,13 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(public auth: AngularFireAuth) {}
+  userData: any = null;
+
+  constructor(public auth: AngularFireAuth) {
+    this.auth.authState.subscribe((user) => {
+      this.userData = user ? user : null;
+    });
+  }
 
   signUp(email: string, password: string) {
     return this.auth.createUserWithEmailAndPassword(email, password);
