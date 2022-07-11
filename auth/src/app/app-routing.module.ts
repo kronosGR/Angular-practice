5655f21df12d87c240b17d12cba25d2e9e4da765
kronosGR <geo.elgeo@gmail.com>
2022-07-11 07:28:46 +0200

    added router guards

--- auth/src/app/app-routing.module.ts
+++ auth/src/app/app-routing.module.ts
@@ -4,11 +4,20 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { SecretComponent } from './secret/secret.component';
 import { LogoutComponent } from './logout/logout.component';
+import { AngularFireAuthGuard } from '@angular/fire/compat/auth-guard';
+import { redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+
+const authOnly = () => redirectUnauthorizedTo(['']);
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'secret', component: SecretComponent },
+  {
+    path: 'secret',
+    component: SecretComponent,
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: authOnly },
+  },
   { path: 'logout', component: LogoutComponent },
 ];
 
