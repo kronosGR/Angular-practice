da8429d9036f2f3dfc1de86a18f26955bd250e81
kronosGR <geo.elgeo@gmail.com>
2022-07-13 12:19:10 +0200

    added menu

--- IonicPracticeProject/src/app/app.component.ts
+++ IonicPracticeProject/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  appPages = [
+    {
+      title: 'Home',
+      url: '/home',
+      icon: 'home',
+    },
+    {
+      title: 'Roster',
+      url: '/roster',
+      icon: 'people',
+    },
+  ];
+  constructor(private platform: Platform) {}
+  ngOnInit(): void {}
 }
