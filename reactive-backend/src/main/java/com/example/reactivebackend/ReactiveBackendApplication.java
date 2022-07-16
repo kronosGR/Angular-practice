974d242462ccc5736a1561b427c6da407f2c44fd
kronosGR <geo.elgeo@gmail.com>
2022-07-16 07:20:58 +0200

    added Quote class

--- reactive-backend/src/main/java/com/example/reactivebackend/ReactiveBackendApplication.java
+++ reactive-backend/src/main/java/com/example/reactivebackend/ReactiveBackendApplication.java
@@ -2,6 +2,9 @@ package com.example.reactivebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ReactiveBackendApplication {
@@ -9,5 +12,12 @@ public class ReactiveBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveBackendApplication.class, args);
 	}
+}
 
+@RestController
+class GreetingController{
+	@RequestMapping("/hello/{name}")
+	String hello(@PathVariable String name){
+		return "Hello, "+ name+"!";
+	}
 }
